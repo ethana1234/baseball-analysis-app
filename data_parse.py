fd66195064843e2c37451dbfcb3d60eccb279dee
Ethan Agranoff <ethana1234@gmail.com>
2020-10-05 16:31:13 -0600

    Switched to pandas data cleaning

--- data_parse.py
+++ data_parse.py
@@ -1,5 +1,10 @@
-import sys,requests,sqlite3,json
+import sys,requests,sqlite3,json,hashlib
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
 from requests.exceptions import HTTPError
+
+sql_max_int = 2147483647
 
 def db_setup():
     # Connect to db
@@ -15,6 +20,7 @@ def db_setup():
 
 def db_error_cleanup(conn, e):
     # Proper cleanup of db after catching an Exception
+    conn.rollback()
     conn.cursor().close()
     conn.close()
     raise e
@@ -30,214 +36,118 @@ def create_baseball_tables(conn):
     queries = []
     # Setup the tables on the db if not done already
     queries.append('''
-    CREATE TABLE IF NOT EXISTS Batting (
+    CREATE TABLE IF NOT EXISTS Team (
+        id integer PRIMARY KEY,
+        name string NOT NULL,
+        team_code string NOT NULL,
+        league string NOT NULL,
+        division string NOT NULL,
+        wins integer DEFAULT 0,
+        losses integer DEFAULT 0
+    )''')
+    queries.append('''
+    CREATE TABLE IF NOT EXISTS BattingGame (
         game_id integer NOT NULL,
         team_id integer NOT NULL,
-        fly_outs integer,
-        ground_outs integer,
-        runs integer,
-        singles integer,
-        doubles integer,
-        triples integer,
-        home_runs integer,
-        strike_outs integer,
-        walks integer,
-        intentional_walks integer,
-        hits integer,
-        hit_by_pitch integer,
-        BA real,
+        Date string,
+        HomeAway string,
+        Opp string,
+        Result string,
+        RunsAgainst integer,
+        PA integer,
         AB integer,
+        R integer,
+        H integer,
+        "2B" integer,
+        "3B" integer,
+        HR integer,
+        RBI integer,
+        BB integer,
+        IBB integer,
+        SO integer,
+        HBP integer,
+        SH integer,
+        SF integer,
+        ROE integer,
+        GDP integer,
+        SB integer,
+        CS integer,
+        LOB integer,
+        PlayersUsed integer,
+        BA real,
         OBP real,
         SLG real,
         OPS real,
-        caught_stealing integer,
-        bases_stolen integer,
-        stolen_base_percentage real,
-        ground_into_double_play integer,
-        ground_into_triple_play integer,
-        plate_appearances integer,
-        total_bases integer,
-        RBI integer,
-        LOB integer,
-        sac_bunts integer,
-        sac_flies integer,
-        catchers_interference integer,
-        pickoffs integer,
-        PRIMARY KEY(game_id, team_id)
+        OppStarterThr string,
+        PRIMARY KEY (game_id, team_id),
+        FOREIGN KEY(team_id) REFERENCES Team(id)
     )''')
-    queries.append('''
-    CREATE TABLE IF NOT EXISTS Pitching (
-        game_id integer NOT NULL,
-        team_id integer NOT NULL,
-        ground_outs integer,
-        air_outs integer,
-        runs integer,
-        singles integer,
-        doubles integer,
-        triples integer,
-        home_runs integer,
-        strike_outs integer,
-        walks integer,
-        intentional_walks integer,
-        hits integer,
-        hit_by_pitch integer,
-        AB integer,
-        OBP real,
-        caught_stealing integer,
-        stolen_bases integer,
-        stolen_base_percentage real,
-        ERA real,
-        IP real,
-        save_oppurtunities integer,
-        earned_runs integer,
-        WHIP real,
-        batter_faced integer,
-        outs integer,
-        complete_games integer,
-        shutouts integer,
-        balks integer,
-        wild_pitches integer,
-        pickoffs integer,
-        RBI integer,
-        inherited_runners integer,
-        inherited_runners_scored integer,
-        catchers_interference integer,
-        sac_bunts integer,
-        sac_flies integer,
-        PRIMARY KEY(game_id, team_id)
-    )''')
+
     for query in queries:
         create_table(conn, query)
 
-def fetch_gamefeed(game_id):
-    # Retrieve JSON pitch data from Baseball Savant API
-    url = f'https://baseballsavant.mlb.com/gf?game_pk={game_id}'
+def clean_batting_data(data, team_code):
+    #   Rename some of the columns
+    data.rename(columns={'Unnamed: 3':'HomeAway', 'Thr':'OppStarterThr', '#':'PlayersUsed'}, inplace=True)
+    #   Drop place holder rows 
+    data.drop(data[data['OBP'] == 'OBP'].index, inplace=True)
+    #   Fix Home/Away column values
+    data.replace({'HomeAway': {'@':'A'}}, inplace=True)
+    data['HomeAway'].fillna('H', inplace=True)
+    #   Split result column into multiple columns
+    data[['Result', 'RunsAgainst']] = data['Rslt'].str.split(',', expand=True)
+    data['RunsAgainst'] = data['RunsAgainst'].str.split('-').str[1]
+    #   Fix Date column for double headers
+    data['Date'] = data['Date'].str.slice(stop=5)
+    #   Generate unique game id for each game and make it the index
+    game_ids = [data['Date'].iloc[i] + ((data['Opp'].iloc[i] + team_code) if data['HomeAway'].iloc[i] == 'H' else (team_code + data['Opp'].iloc[i])) + data['Opp. Starter (GmeSc)'].iloc[i] for i in range(len(data.index))]
+    game_ids = [int(hashlib.sha256(s.encode('utf-8')).hexdigest(), 16) % sql_max_int for s in game_ids]
+    data['game_id'] = game_ids
+    data.set_index('game_id', inplace=True)
+    # Add team id based on 3 letter team code
+    team_ids = [sum([ord(char) for char in team_code]) for i in range(len(data.index))]
+    data['team_id'] = team_ids
+    #   Drop unneccessary columns and reorder the remains
+    data.drop(columns=['Rslt', 'Rk', 'Gtm', 'Opp. Starter (GmeSc)'], inplace=True)
+    data = data[['team_id', 'Date', 'HomeAway', 'Opp', 'Result', 'RunsAgainst', 'PA', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'BB', 'IBB', 'SO', 'HBP', 'SH', 'SF',
+                'ROE', 'GDP', 'SB', 'CS', 'LOB', 'PlayersUsed', 'BA', 'OBP', 'SLG', 'OPS', 'OppStarterThr']]
+    #   Convert numeric columns to numeric types
+    data.loc[:, 'RunsAgainst':'PlayersUsed'] = data.loc[:, 'RunsAgainst':'PlayersUsed'].apply(pd.to_numeric)
+    data.loc[:, 'BA':'OPS'] = data.loc[:, 'BA':'OPS'].apply(pd.to_numeric)
+
+    return data
+
+
+def insert_batting_team_data(conn, team_code):
+    # Retrieve JSON pitch data from Baseball Reference
+    url = f'https://www.baseball-reference.com/teams/tgl.cgi?team={team_code}&t=b&year=2020'
     try:
         r = requests.get(url)
         # If the response was successful, no exception will be raised
         r.raise_for_status()
     except HTTPError as http_err:
         raise http_err
-    data = r.json()
-    return data
 
-def insert_batting(conn, data, game_id):
-    batting_query = []
-    for team in ['home', 'away']:
-        batting_data = data[team]['teamStats']['batting']
-        singles = batting_data['hits'] - (batting_data['doubles'] + batting_data['triples'] + batting_data['homeRuns'])
-        for stat in ['avg', 'obp', 'slg', 'ops', 'stolenBasePercentage']:
-            batting_data[stat] = float(batting_data[stat]) if batting_data[stat] != '.---' else None
-        batting_data = list(batting_data.values())[:-1]
-        batting_query.append((game_id, data[team]['team']['id'], *batting_data, singles))
-    # Insert into Batting table for game
-    batting_box_score = '''
-    INSERT INTO Batting (
-        game_id,
-        team_id,
-        fly_outs,
-        ground_outs,
-        runs,
-        doubles,
-        triples,
-        home_runs,
-        strike_outs,
-        walks,
-        intentional_walks,
-        hits,
-        hit_by_pitch,
-        BA,
-        AB,
-        OBP,
-        SLG,
-        OPS,
-        caught_stealing,
-        bases_stolen,
-        stolen_base_percentage,
-        ground_into_double_play,
-        ground_into_triple_play,
-        plate_appearances,
-        total_bases,
-        RBI,
-        LOB,
-        sac_bunts,
-        sac_flies,
-        catchers_interference,
-        pickoffs,
-        singles)
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
+    # Retrieve Batting Table from XML page and put into a DataFrame
+    soup = BeautifulSoup(r.content, "lxml")
+    table = soup.find('table')
+    data = pd.read_html(str(table))[0]
+
+    clean_data = clean_batting_data(data, team_code)
+
     try:
-        conn.cursor().executemany(batting_box_score, batting_query)
+        clean_data.to_sql('BattingGame', conn, if_exists='append')
         conn.commit()
     except sqlite3.IntegrityError:
-        print('Game already added')
+        print('Team already added')
     except Exception as e:
         db_error_cleanup(conn, e)
 
-def insert_pitching(conn, data, game_id):
-    pitching_query = []
-    for team in ['home', 'away']:
-        pitching_data = data[team]['teamStats']['pitching']
-        singles = pitching_data['hits'] - (pitching_data['doubles'] + pitching_data['triples'] + pitching_data['homeRuns'])
-        for stat in ['obp', 'era', 'inningsPitched', 'stolenBasePercentage', 'whip']:
-            pitching_data[stat] = float(pitching_data[stat]) if pitching_data[stat] != '.---' else None
-        for stat in ['hitBatsmen', 'groundOutsToAirouts', 'runsScoredPer9', 'homeRunsPer9']:
-            pitching_data.pop(stat)
-        pitching_data = list(pitching_data.values())
-        pitching_query.append((game_id, data[team]['team']['id'], *pitching_data, singles))
-    # Insert into Pitching table for game
-    pitching_box_score = '''
-    INSERT INTO Pitching (
-        game_id,
-        team_id,
-        ground_outs,
-        air_outs,
-        runs,
-        doubles,
-        triples,
-        home_runs,
-        strike_outs,
-        walks,
-        intentional_walks,
-        hits,
-        hit_by_pitch,
-        AB,
-        OBP,
-        caught_stealing,
-        stolen_bases,
-        stolen_base_percentage,
-        ERA,
-        IP,
-        save_oppurtunities,
-        earned_runs,
-        WHIP,
-        batter_faced,
-        outs,
-        complete_games,
-        shutouts,
-        balks,
-        wild_pitches,
-        pickoffs,
-        RBI,
-        inherited_runners,
-        inherited_runners_scored,
-        catchers_interference,
-        sac_bunts,
-        sac_flies,
-        singles)
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
-    try:
-        conn.cursor().executemany(pitching_box_score, pitching_query)
-        conn.commit()
-    except sqlite3.IntegrityError:
-        print('Game already added')
-    except Exception as e:
-        db_error_cleanup(conn, e)
+def insert_team_data(conn, team_code):
+    pass
 
-db = db_setup()
-create_baseball_tables(db)
 
-game_id = 631152
-data = fetch_gamefeed(game_id)
-insert_batting(db, data['boxscore']['teams'], game_id)
-insert_pitching(db, data['boxscore']['teams'], game_id)
+conn = db_setup()
+create_baseball_tables(conn)
+insert_batting_team_data(conn, 'PHI')
+insert_team_data(conn, 'PHI')
